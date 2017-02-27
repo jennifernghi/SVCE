6f6458d83bb3d402265549cd16e6851fd3f0bc1c
jennifernghinguyen <jennifernghi@yahoo.com>
2017-02-26 23:49:32 -0800

    temp UI for add ideas ui

--- app/src/main/java/com/example/android/svce/activities/AddNewIdea.java
+++ app/src/main/java/com/example/android/svce/activities/AddNewIdea.java
@@ -2,6 +2,8 @@ package com.example.android.svce.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.android.svce.R;
 
@@ -11,5 +13,14 @@ public class AddNewIdea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_idea);
+
+        Spinner spinner = (Spinner) findViewById(R.id.category);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
